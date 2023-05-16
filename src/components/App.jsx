fb203ea2c4ceeb3aa5b795c7fbd68f6186f3d9b7
ViktoriyaSF <kievstar01051985@gmail.com>
2023-05-16 22:03:14 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -7,14 +7,70 @@ import { FeedbackOptions } from './FeedbackOptions/FeedbackOptions';
 import { Statistics } from './Statistics/Statistics';
 import { Notification } from 'components/Notification/Notification';
 
-export class App extends Component {
-  state = {
-    good: 0,
-    neutral: 0,
-    bad: 0,
-  };
+// export class App extends Component {
+//   state = {
+//     good: 0,
+//     neutral: 0,
+//     bad: 0,
+//   };
 
-  //щоб рахувалась кнопка
+//   //щоб рахувалась кнопка
+//   onLeaveFeedback = evt => {
+//     const { name } = evt.target;
+//     this.setState(prevState => ({ [name]: prevState[name] + 1 }));
+//   };
+
+//   //підрахунок загальної суми
+//   countTotalFeedback = () => {
+//     return Object.values(this.state).reduce((previousValue, number) => {
+//       return previousValue + number;
+//     }, 0);
+//   };
+
+//   //підрахунок %% позитивних відгуків
+//   countPositiveFeedbackPercentage = () => {
+//     return this.state.good !== 0
+//       ? Math.round((this.state.good / this.countTotalFeedback()) * 100)
+//       : 0;
+//   };
+
+//   render() {
+//     const { good, neutral, bad } = this.state;
+
+//     return (
+//       <Layout>
+//         <Section title="Please leave feedback">
+//           <FeedbackOptions
+//             options={Object.keys(this.state)}
+//             onLeaveFeedback={this.onLeaveFeedback}
+//           />
+//         </Section>
+//         <Section title="Statistics">
+//           {this.countTotalFeedback() ? (
+//             <Statistics
+//               good={good}
+//               neutral={neutral}
+//               bad={bad}
+//               total={this.countTotalFeedback()}
+//               positivePercentage={this.countPositiveFeedbackPercentage()}
+//             ></Statistics>
+//           ) : (
+//             <Notification message="There is no feedback" />
+//           )}
+//         </Section>
+//         <GlobalStyle />
+//       </Layout>
+//     );
+//   }
+// }
+export const App = () => {
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0)
+
+
+
+    //щоб рахувалась кнопка
   onLeaveFeedback = evt => {
     const { name } = evt.target;
     this.setState(prevState => ({ [name]: prevState[name] + 1 }));
@@ -34,7 +90,7 @@ export class App extends Component {
       : 0;
   };
 
-  render() {
+    render() {
     const { good, neutral, bad } = this.state;
 
     return (
